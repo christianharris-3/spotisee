567263ecdece5d092afdb5b39a5e14feea9e4c04
CBrocks <chris.brocklebank1@gmail.com>
2026-07-31 23:47:21 +0100

    continued docker setup

--- backend/src/main/java/com/spotisee/app/Spotisee.java
+++ backend/src/main/java/com/spotisee/app/Spotisee.java
@@ -5,7 +5,10 @@ import com.spotisee.app.dao.DataPreProcessingDao;
 import com.spotisee.app.dao.UploadDao;
 import com.spotisee.app.resources.LoginResource;
 import com.spotisee.app.resources.UploadDataResource;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 import io.dropwizard.db.ManagedDataSource;
 import org.jdbi.v3.core.Jdbi;
@@ -18,20 +21,34 @@ public class Spotisee extends Application<AppConfiguration> {
     }
 
     @Override
-    public void run(AppConfiguration configuration,
-                    Environment environment) {
-
-        final ManagedDataSource dataSource = configuration.getDatabase().build(
-                environment.metrics(), "db"
+    public void initialize(Bootstrap<AppConfiguration> bootstrap) {
+        bootstrap.setConfigurationSourceProvider(
+            new SubstitutingSourceProvider(
+                bootstrap.getConfigurationSourceProvider(),
+                new EnvironmentVariableSubstitutor(false)
+            )
         );
+    }
+
+    @Override
+    public void run(
+        AppConfiguration configuration,
+        Environment environment
+    ) {
+        final ManagedDataSource dataSource = configuration
+            .getDatabase()
+            .build(environment.metrics(), "db");
 
         final Jdbi jdbi = Jdbi.create(dataSource);
         jdbi.installPlugin(new SqlObjectPlugin());
 
         UploadDao uploadDao = jdbi.onDemand(UploadDao.class);
-        DataPreProcessingDao dataPreProcessingDao = jdbi.onDemand(DataPreProcessingDao.class);
+        DataPreProcessingDao dataPreProcessingDao =
+            jdbi.onDemand(DataPreProcessingDao.class);
 
         environment.jersey().register(new LoginResource());
-        environment.jersey().register(new UploadDataResource(uploadDao, dataPreProcessingDao));
+        environment.jersey().register(
+            new UploadDataResource(uploadDao, dataPreProcessingDao)
+        );
     }
 }
